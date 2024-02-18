0a17788aa4b53ae78196c178ee73a69b7d74c052
Sheikh Nazmul islam <152971502+NazmulNiR1@users.noreply.github.com>
2024-02-19 00:04:29 +0600

    finial edd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["*"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
 };
